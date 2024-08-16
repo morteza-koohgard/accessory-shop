c91aaf2b273984ef85efa172f490ccf33360ef68
morteza-koohgard <morteza73.koohgard@gmail.com>
2024-08-16 22:27:00 +0330

    add user test

---
+++ laravel-backend/tests/Feature/UserTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_user_db_fields(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'test user',
+            'email' => 'test user email',
+            'password' => 'test user password',
+            'mobile' => +1234567890,
+        ]);
+
+        $this->assertDatabaseHas('users', [
+            'name' => 'test user',
+            'email' => 'test user email',
+            'password' => 'test user password',
+            'mobile' => +1234567890,
+        ]);
+    }
+
+    public function test_register_user_api(): void
+    {
+        $response = $this->post('/api/register', [
+            'name' => 'test user',
+            'email' => 'test user email',
+            'password' => 'test user password'
+        ])->assertStatus(201);
+
+        $response->assertJsonStructure([
+            'data' => [
+                'name',
+                'email',
+            ],
+        ]);
+    }
+}
