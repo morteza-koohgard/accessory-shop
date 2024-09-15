681e31c759edbbced948650a83854ffc9924fa19
morteza-koohgard <morteza73.koohgard@gmail.com>
2024-09-15 23:19:25 +0330

    add theme switcher component

---
+++ nuxt-admin/components/layouts/ThemeSwitcher.vue
@@ -0,0 +1,17 @@
+<template>
+    <UToggle color="gray" :ui="{ inactive: 'dark:bg-gray-400' }" @click="toggle" size="lg" on-icon="i-heroicons-sun" off-icon="i-heroicons-moon" :model-value="theme" />
+</template>
+
+<script setup>
+const colorMode = useColorMode()
+const theme = ref(false)
+const toggle = () => {
+    if (colorMode.preference == 'dark') {
+        colorMode.preference = 'light'
+        theme.value = true
+    } else {
+        colorMode.preference = 'dark'
+        theme.value = false
+    }
+}
+</script>
