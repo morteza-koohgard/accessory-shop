63a222e2bd6577cff76fae602a421468580ea0ae
morteza-koohgard <morteza73.koohgard@gmail.com>
2024-09-15 23:20:06 +0330

    Refactor Nuxt admin layout: Replace old layout with new default layout

---
+++ nuxt-admin/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="bg-gray-50 dark:bg-gray-800 min-h-screen">
+        <!-- header section -->
+        <LayoutsHeaderSection />
+        <!-- left dashboard panel -->
+        <!-- main content -->
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
